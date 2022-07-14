402abab338b8e03e5e4f6bb3fbddd7234e2d0c7d
Ravena <milenabmendonca@gmail.com>
2022-07-14 17:40:47 -0300

    First commit

---
+++ projetoIDES.java
@@ -0,0 +1,15 @@
+
+public class projetoIDES extends Gato {
+	Gato gato = new Gato(gato);
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float a = 2;
+		float b = 3;
+
+		System.out.println("Hello World! " + (a + b) / 2);
+
+	}
+
+}
